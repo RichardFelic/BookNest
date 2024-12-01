a34dfca57a5a2f57ae64e95276a9d96d5811ea5d
RichardFelic <felricharde42@gmail.com>
2024-12-01 00:42:25 -0400

    feat: configuracion inicial de la app

--- src/main/java/com/richardfeliciano/booknest/BooknestApplication.java
+++ src/main/java/com/richardfeliciano/booknest/BooknestApplication.java
@@ -1,13 +1,31 @@
 package com.richardfeliciano.booknest;
 
+import com.richardfeliciano.booknest.repository.AuthorRepository;
+import com.richardfeliciano.booknest.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.richardfeliciano.booknest.client.Client;
 @SpringBootApplication
-public class BooknestApplication {
+@ComponentScan(basePackages = "com.richardfeliciano.booknest")
+public class BooknestApplication implements CommandLineRunner {
+
+	@Autowired
+	private BookRepository 	bookRepository;
+
+	@Autowired
+	private AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooknestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Client client = new Client(bookRepository, authorRepository);
+		client.start();
+	}
 }
